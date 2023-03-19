309691c6c44f413ee834368bdff71eb0c4d93743
Ryan Rizeq <ryanrizeq@gmail.com>
2023-03-19 11:14:40 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    function findWin(element) {
+        return (element.result === 'W');
+    }
+    if (record.find(findWin)) {
+        return record.find(findWin).year;
+    }
+}
